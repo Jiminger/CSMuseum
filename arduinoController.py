90c8173c198cff3202b1d25654a6b27182f3c3b7
Jiminger <jamespritchie.93@gmail.com>
2022-02-16 11:21:13 -0500

    Testing Python Code

--- arduinoController.py
+++ arduinoController.py
@@ -1,1 +1,25 @@
-print('Hello World')
+import serial
+
+
+if __name__ == '__main__':
+
+    ser = serial.Serial('/dev/tty.usbmodem14101', 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+
+        user = input("Enter input: ")
+        if user == '1':
+            x = '0'
+            y = '60'
+            ser.write(bytes(x, 'utf-8'))
+            ser.write(bytes(y, 'utf-8'))
+        elif user == '2':
+            x = '5'
+            y = '10'
+            ser.write(bytes(x, 'utf-8'))
+            ser.write(bytes(y, 'utf-8'))
+        elif user == '0':
+            x = '0'
+            y = '20'
+            ser.write(bytes(x, 'utf-8'))
+            ser.write(bytes(y, 'utf-8'))
