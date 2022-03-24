dc9fdb47140b64d29d01426b6194fc596f5ecdd2
Jiminger <jamespritchie.93@gmail.com>
2022-03-24 19:59:06 -0400

    finish arduinoController to begin testing

--- arduinoController.py
+++ arduinoController.py
@@ -9,20 +9,31 @@ ser3 = serial.Serial('/dev/tty.usbmodem14101', 9600, timeout=1)
 ser3.reset_input_buffer()
 
 
-def light_entire_museum():
-    def light_cases(case_number):
-        if case_number == "case_one":
-            pass
-        elif case_number == "case_two":
-            pass
-        elif case_number == "case_three":
-            indices = get_item_indexes("case_three", 1)
-            ser3.write(bytes(str(indices[0]), 'utf-8'))
-            ser3.write(bytes(str(indices[1]), 'utf-8'))
+def turn_on_case(case_number):
+    indices = get_item_indexes(case_number, 1)
+    if case_number == "case_one":
+        pass
+    elif case_number == "case_two":
+        pass
+    elif case_number == "case_three":
+        ser3.write(bytes(str(indices[1]), 'utf-8'))
+        ser3.write(bytes(str(indices[2]), 'utf-8'))
 
-    thread_one = Thread(target=light_cases("case_one"))
-    thread_two = Thread(target=light_cases("case_two"))
-    thread_three = Thread(target=light_cases("case_three"))
+
+def turn_off_case(case_number):
+    if case_number == "case_one":
+        pass
+    elif case_number == "case_two":
+        pass
+    elif case_number == "case_three":
+        ser3.write(bytes('-1', 'utf-8'))
+        ser3.write(bytes('-1', 'utf-8'))
+
+
+def light_entire_museum():
+    thread_one = Thread(target=turn_on_case("case_one"))
+    thread_two = Thread(target=turn_on_case("case_two"))
+    thread_three = Thread(target=turn_on_case("case_three"))
 
     thread_one.start()
     thread_two.start()
@@ -31,6 +42,30 @@ def light_entire_museum():
     thread_one.join()
     thread_two.join()
     thread_three.join()
+
+
+def light_specific_case(case_number):
+    if case_number == "case_one":
+        pass
+    elif case_number == "case_two":
+        pass
+    elif case_number == "case_three":
+        turn_off_case("case_one")
+        turn_off_case("case_two")
+        turn_on_case("case_three")
+
+
+def light_specific_item(case_number, item_id):
+    indices = get_item_indexes(case_number, item_id)
+    if case_number == "case_one":
+        pass
+    elif case_number == "case_two":
+        pass
+    elif case_number == "case_three":
+        turn_off_case("case_one")
+        turn_off_case("case_two")
+        ser3.write(bytes(str(indices[1]), 'utf-8'))
+        ser3.write(bytes(str(indices[2]), 'utf-8'))
 
 
 def get_item_indexes(case_number, item_id):
@@ -52,23 +87,3 @@ def get_item_indexes(case_number, item_id):
 
     return case_number, my_result[0], my_result[1]
 
-
-"""
-def lightController():
-    user = input("Enter input: ")
-    if user == '1':
-        x = '0'
-        y = '60'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-    elif user == '2':
-        x = '5'
-        y = '10'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-    elif user == '0':
-        x = '0'
-        y = '20'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-"""
