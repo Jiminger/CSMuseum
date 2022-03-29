60f70d3c6886a943fd4c00ceef15f0bc2c9455cc
Jiminger <jamespritchie.93@gmail.com>
2022-03-29 11:24:58 -0400

    adding paths in flask app to case and item webpages

--- app.py
+++ app.py
@@ -1,33 +1,9 @@
 from flask import Flask
 from flask import render_template
 
-import serial
 import arduino_controller as controller
 
 app = Flask(__name__)
-
-# Commented out for testing of the arduinoController class
-# ser = serial.Serial('/dev/tty.usbmodem14101', 9600, timeout=1)
-# ser.reset_input_buffer()
-
-"""
-def lightController(user):
-    if user == '1':
-        x = '0'
-        y = '60'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-    elif user == '2':
-        x = '5'
-        y = '10'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-    elif user == '0':
-        x = '0'
-        y = '20'
-        ser.write(bytes(x, 'utf-8'))
-        ser.write(bytes(y, 'utf-8'))
-"""
 
 
 @app.route('/')
@@ -36,22 +12,19 @@ def index():
     return render_template("index.html")
 
 
-# TODO : route to other web pages
-"""
-@app.route('/case_one')
-def case1():
-    lightController("2")
-    return 'case_1'
+# Case One Pages
+@app.route('/case_1')
+def case_1():
+    controller.turn_on_case(1)
 
 
-@app.route('/case_two')
-def case2():
-    lightController("0")
-    return 'case_two'
+# Case Two Pages
+@app.route('/case_2')
+def case_2():
+    controller.turn_on_case(2)
 
 
-@app.route('/case_three')
-def case2():
-    lightController("0")
-    return 'case_3'
-"""
+# Case Three Pages
+@app.route('/case_3')
+def case_3():
+    controller.turn_on_case(3)
