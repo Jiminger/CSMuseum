0ef1ad44203f23d7497b48fdbaa674c9b142083a
Jiminger <jamespritchie.93@gmail.com>
2022-03-29 21:17:54 -0400

    changs made to flask app and templates for testing

--- app.py
+++ app.py
@@ -9,42 +9,30 @@ app = Flask(__name__)
 # Index Page
 @app.route('/')
 def index():
-    controller.light_entire_museum()
+    #controller.light_entire_museum()
     return render_template("index.html")
-
 
 # Case One Pages
 @app.route('/case_1/')
 def case_1():
-    controller.turn_on_case(1)
+    #controller.turn_on_case(1)
     return render_template("case_1.html")
-
-@app.route('/case_1/<id>')
-def get_item(id):
-    return id
 
 # Case Two Pages
 @app.route('/case_2/')
 def case_2():
-    controller.turn_on_case(2)
+    #controller.turn_on_case(2)
     return render_template("case_2.html")
-
-@app.route('/case_2/<id>')
-def get_item(id):
-    return id
-
 
 # Case Three Pages
 @app.route('/case_3/')
 def case_3():
-    controller.turn_on_case(3)
+    #controller.turn_on_case(3)
     return render_template("case_3.html")
 
-@app.route('/case_3/<id>')
-def get_item(id):
-    return id
-
-
+@app.route('/case_<case_id>/<item_id>')
+def item_page(case_id,item_id):
+    return case_id + " --> " + item_id
 
 if __name__ == "__main__":
     app.run()
