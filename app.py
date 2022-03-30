3e262d458893d010b6c389fc8add5b5f5d220e8b
Jiminger <jamespritchie.93@gmail.com>
2022-03-29 21:02:54 -0400

    more template additions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-import arduino_controller as controller
+#import arduino_controller as controller
 
 app = Flask(__name__)
 
@@ -14,21 +14,37 @@ def index():
 
 
 # Case One Pages
-@app.route('/case_1')
+@app.route('/case_1/')
 def case_1():
     controller.turn_on_case(1)
     return render_template("case_1.html")
 
+@app.route('/case_1/<id>')
+def get_item(id):
+    return id
 
 # Case Two Pages
-@app.route('/case_2')
+@app.route('/case_2/')
 def case_2():
     controller.turn_on_case(2)
     return render_template("case_2.html")
 
+@app.route('/case_2/<id>')
+def get_item(id):
+    return id
+
 
 # Case Three Pages
-@app.route('/case_3')
+@app.route('/case_3/')
 def case_3():
     controller.turn_on_case(3)
     return render_template("case_3.html")
+
+@app.route('/case_3/<id>')
+def get_item(id):
+    return id
+
+
+
+if __name__ == "__main__":
+    app.run()
