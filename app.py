a90949191f1eb8d3d0d0bf2a018d25709268df4a
Jiminger <jamespritchie.93@gmail.com>
2022-04-03 12:45:25 -0400

    add item template functionality, ready for test

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
 
-#import arduino_controller as controller
+# import arduino_controller
+# import db_controller
 
 app = Flask(__name__)
 
@@ -9,30 +10,36 @@ app = Flask(__name__)
 # Index Page
 @app.route('/')
 def index():
-    #controller.light_entire_museum()
+    # controller.light_entire_museum()
     return render_template("index.html")
+
 
 # Case One Pages
 @app.route('/case_1/')
 def case_1():
-    #controller.turn_on_case(1)
+    # controller.turn_on_case(1)
     return render_template("case_1.html")
+
 
 # Case Two Pages
 @app.route('/case_2/')
 def case_2():
-    #controller.turn_on_case(2)
+    # controller.turn_on_case(2)
     return render_template("case_2.html")
+
 
 # Case Three Pages
 @app.route('/case_3/')
 def case_3():
-    #controller.turn_on_case(3)
+    # controller.turn_on_case(3)
     return render_template("case_3.html")
 
-@app.route('/case_<case_id>/<item_id>')
-def item_page(case_id,item_id):
-    return case_id + " --> " + item_id
+
+@app.route('/case_<case_id>/item_<item_id>')
+def item_page(case_id, item_id):
+    item_info = db_controller.get_item_information(case_id,item_id)
+    return render_template("item.html", item_name=item_info[0], item_desc=item_info[1], img_path=item_info[2])
+
 
 if __name__ == "__main__":
     app.run()
--- db_controller.py
+++ db_controller.py
@@ -23,10 +23,10 @@ def format_input(start_index, end_index):
     return '<' + str(start_index) + ',' + str(end_index) + '>'
 
 
-def get_item_indexes(case_number, item_id):
+def get_item_indexes(case_id, item_id):
     connection = open_connection()
     my_cursor = connection[1]
-    my_cursor.execute("SELECT start_index, end_index FROM " + case_number + " WHERE item_id =" + str(item_id))
+    my_cursor.execute("SELECT start_index, end_index FROM Items WHERE case_id=" + str(case_id) + "AND item_id =" + str(item_id))
 
     my_result = my_cursor.fetchall()
 
@@ -34,10 +34,10 @@ def get_item_indexes(case_number, item_id):
     return format_input(str(my_result[0][0]), str(my_result[0][1]))
 
 
-def get_item_information(case_number, item_id):
+def get_item_information(case_id, item_id):
     connection = open_connection()
     my_cursor = connection[1]
-    my_cursor.execute("SELECT item_name, item_desc FROM " + case_number + " WHERE item_id =" + str(item_id))
+    my_cursor.execute("SELECT item_name, item_desc, img_path FROM Items WHERE case_id =" + str(case_id) + " AND  item_id =" + str(item_id))
 
     my_result = my_cursor.fetchall()
     close_connection(connection)
