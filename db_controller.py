45637336183e3952bb5c49de20f75865c46416b4
Jiminger <jamespritchie.93@gmail.com>
2022-04-11 14:34:22 -0400

    syntax fix

--- db_controller.py
+++ db_controller.py
@@ -26,7 +26,7 @@ def format_input(start_index, end_index):
 def get_item_indexes(case_id, item_id):
     connection = open_connection()
     my_cursor = connection[1]
-    my_cursor.execute("SELECT start_index, end_index FROM Items WHERE case_id=" + str(case_id) +
+    my_cursor.execute("SELECT start_index, end_index FROM Items WHERE case_id= " + str(case_id) +
                       " AND item_id =" + str(item_id) + ";")
 
     my_result = my_cursor.fetchall()
